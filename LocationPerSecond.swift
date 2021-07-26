131d7870b1db91f6145b0df2d7c2d7bc48a389a8
Sodiam <61130754+Sodiam@users.noreply.github.com>
2021-07-26 15:03:28 +0900

    Update LocationPerSecond.swift

--- LocationPerSecond.swift
+++ LocationPerSecond.swift
@@ -1,6 +1,7 @@
+var spotlist // 位置情報データをSpotオブジェクトに変換したものを格納する配列。Spotの定義はここでは省略
+
 struct LocationManager:  NSObject, ObservableObject, CLLocationManagerDelegate {
     //各メンバは省略
-
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         locations.last.map {
             let center = CLLocationCoordinate2D(
@@ -17,11 +18,10 @@ struct LocationManager:  NSObject, ObservableObject, CLLocationManagerDelegate {
     }
     
     func appendtospotlist() {
-        spotlist.append(Spot(latitude: self.region.center.latitude, longitude: self.region.center.longitude))
+        spotlist.append(Spot(latitude: self.region.center.latitude, longitude: self.region.center.longitude)) //Spotは緯度と経度の情報を持つ
     }
 }
 
-// in Viewfile
 struct ContentView: View {
     //メンバ省略
     var body: some View {
@@ -29,7 +29,7 @@ struct ContentView: View {
     }
     .onAppear() {
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-            if (self.tracking) {
+            if (self.tracking/* 制御変数 */) {
                 self.manager.appendtospotlist()
             }
         }
